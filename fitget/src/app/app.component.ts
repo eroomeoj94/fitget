796b1d79092141a00babce4986c1e4adadf3d9ae
PT\JoeMoore <joe@purpletuesday.com>
2017-07-05 13:53:07 +0100

    Install an OAuth 2.0 package just need to implement it for Fitbits API

--- fitget/src/app/app.component.ts
+++ fitget/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { OAuthService } from 'angular-oauth2-oidc';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Fitget';
+
+  constructor(private oauthService: OAuthService){
+    //TODO: Implement OAuthService for the fitbit API
+  }
 }
--- fitget/src/app/app.module.ts
+++ fitget/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { OAuthModule } from 'angular-oauth2-oidc';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    OAuthModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
