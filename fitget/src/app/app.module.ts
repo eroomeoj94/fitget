579cee7ac750ff82939a7f7d052be6517f5e0453
Joseph Moore <eroomeoj94@gmail.com>
2017-07-06 13:50:51 +0100

    Created the home and login components and started routing

--- fitget/src/app/app.module.ts
+++ fitget/src/app/app.module.ts
@@ -2,14 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { OAuthModule } from 'angular-oauth2-oidc';
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    OAuthModule.forRoot()
+    HttpModule,
+    OAuthModule.forRoot(),
+    RouterModule.forRoot([{
+      path: 'login',
+      component: LoginComponent
+    },
+    {
+      path: 'home',
+      component: HomeComponent
+    }, {
+      path: '',
+      redirectTo: '/home',
+      pathMatch: 'full'
+    }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
